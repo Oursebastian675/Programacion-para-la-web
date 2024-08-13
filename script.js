d84d632733cd55c556d9b297cefc5a85fb1d81b5
Oursebastian675 <Spedroza@cesde.net>
2024-08-13 08:52:28 -0500

    Var-ParseInt y mas

---
+++ script.js
@@ -0,0 +1,29 @@
+/* datos de salida */
+
+
+/* datos de entrada */
+
+// prompt("Ingrese su nombre: "); /* Scanner en Java */
+
+/* variables JavaScript 
+    *Hay tres tipos de variables (var, let const)
+    *Las variables no tienes tipo de dato
+    *Las tres vaiables almacenan cuaquier tipo de dato
+*/
+
+// var nombreCompleto = "Sebastian Pedroza Naranjo";
+// console.log(nombreCompleto);
+
+
+
+
+
+
+/* datos de proceso */
+var numeroUno = parseInt(prompt("Ingrese un numero :"));
+var numeroDos = parseInt(prompt("Ingrese un numero :"));
+console.log(numeroUno + numeroDos); /* concatenacion */
+
+/* Todo dato ingresado por teclado es por defecto un String 
+    *Parseo de datos (conversion de datos)
+*/
