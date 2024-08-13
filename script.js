fbe70daf4f854a806e5347e2f237aca8efd27339
Oursebastian675 <Spedroza@cesde.net>
2024-08-13 10:05:50 -0500

    Mucha informacion

--- script.js
+++ script.js
@@ -5,13 +5,13 @@
 
 // prompt("Ingrese su nombre: "); /* Scanner en Java */
 
-/* variables JavaScript 
+/* variables JavaScript
     *Hay tres tipos de variables (var, let const)
     *Las variables no tienes tipo de dato
     *Las tres vaiables almacenan cuaquier tipo de dato
 */
 
-// var nombreCompleto = "Sebastian Pedroza Naranjo";
+// var nombreCompleto = "Sebastian Pedroza Naranjo"
 // console.log(nombreCompleto);
 
 
@@ -19,11 +19,63 @@
 
 
 
-/* datos de proceso */
-var numeroUno = parseInt(prompt("Ingrese un numero :"));
-var numeroDos = parseInt(prompt("Ingrese un numero :"));
-console.log(numeroUno + numeroDos); /* concatenacion */
+/* datos de proceso
+operaciones aritmeticas y logicas
+Suma, resta, multiplicacion, division, comparacion, asignacion, incremento, decremento, etc.
 
-/* Todo dato ingresado por teclado es por defecto un String 
+Suma: valorUno + valorDos (+)
+Resta: valorUno - valorDos (-)
+multipliciacion: valorUno * valorDos (*)
+Division: valorUno / valorDos (/)
+
+
+Logicos (Generan un resultado booleano: true o false):
+<   ->
+>   ->
+<=  ->
+>=  ->
+==  ->
+=== ->
+!=  ->
+!== ->
+
+*/
+// console.log(3<3)
+// console.log(3<2)
+
+// console.log(3<=3)
+// console.log(3>=2)
+
+console.log(3 == '3')
+console.log(3 === '3')
+
+
+// var numeroUno = parseInt(prompt("Ingrese un numero :"));
+// var numeroDos = parseInt(prompt("Ingrese un numero :"));
+// console.log(numeroUno + numeroDos); /* concatenacion */
+
+/* Todo dato ingresado por teclado es por defecto un String
     *Parseo de datos (conversion de datos)
 */
+
+// var nombreCompleto = 'Sebastian pedroza'
+// var nombreCompleto = 202020202
+// var saldoCuenta = 20000000
+// var saldoCuenta = 2000
+// console.log(saldoCuenta)
+
+
+// let saldoCuenta = 20000000
+// saldoCuenta = 2000
+// console.log(saldoCuenta)
+
+// const saldoCuenta = 20000000
+// saldoCuenta = 2000
+// console.log(saldoCuenta)
+
+/* Las variables de tippo var permiten sobreescritura de variables y de contenido */
+/* Las variables de tipo let no permiten sobreescritura de variable pero si de contenido */
+/* Las variables de tipo const no permiten sobreescritura de variable ni de contenido */
+/* Las variables de tipo let y const son el tipo nde variables mas utilizado */
+
+/*  */
